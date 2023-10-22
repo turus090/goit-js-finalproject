f69d7b986c149da9e8a7c41b79496481a51fb7a1
Sergii Tatianchenko <taurus090@gmail.com>
2023-10-22 13:40:09 +0300

    fixed simpleBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,22 +6,22 @@ const galleryRef = document.querySelector(".gallery");
 galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup);
 
 function createGalleryMarkup(items) {
-	return items
-		.map(({ preview, original, description }) => {
-			return `
+  return items
+    .map(({ preview, original, description }) => {
+      return `
       <li>
     <a class="gallery__item" href="${original}">
 	<img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
 </li>
     `;
-		})
-		.join("");
+    })
+    .join("");
 }
 
 var lightbox = new SimpleLightbox(".gallery a", {
-	captionsData: "alt",
-	captionDelay: 250,
+  captionsData: "alt",
+  captionDelay: 250,
 });
 
 console.log(galleryItems);
